678884613fe34ff865ccda3609c75db4ab610993
Kapil Sharma <sharma.kapil1306@gmail.com>
2024-05-06 21:58:18 +0200

    no change

---
+++ index.js
@@ -0,0 +1,11 @@
+alert("You are visiting http://127.0.0.1:3000/wd51-group2-cookbook/index.html");
+
+function message() {
+  var txt;
+  if (confirm("Do you want to share the recipe?")) {
+    txt = "Thank you!";
+  } else {
+    txt = "So Sad!";
+  }
+  document.getElementById("alert").innerHTML = txt;
+}
